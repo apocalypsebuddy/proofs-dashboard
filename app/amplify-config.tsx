b2cde92ff10288b567c8fd4ae73d7cf94a1a43fd
Mathias Wolf <mathias.g.wolf@gmail.com>
2025-04-28 15:52:01 -0700

    fix missing cognito keys during build time

--- app/amplify-config.tsx
+++ app/amplify-config.tsx
@@ -3,8 +3,12 @@
 import { Amplify } from "aws-amplify";
 
 // Check if required environment variables are defined
-const userPoolId = process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID;
-const userPoolClientId = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID;
+// const userPoolId = process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID;
+// const userPoolClientId = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID;
+// Envvars are not available during build time in AWS App Runner (stupid)
+// The Cognito keys are public so it's safe to expose them here. This should be a temporary workaround
+const userPoolId = 'us-west-2_wblQBOMpC';
+const userPoolClientId = '3ijjn66958hi0trhrpqch9oscp';
 
 if (!userPoolId || !userPoolClientId) {
   throw new Error('Missing required environment variables for Cognito configuration');
