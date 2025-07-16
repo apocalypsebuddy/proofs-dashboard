f1216ceb4cb8d9c5733d6be2f8190e61c8ba4771
Mathias Wolf <mathias.g.wolf@gmail.com>
2025-07-15 19:07:11 -0700

    allow deletion of dummy data

--- app/api/proofs/[id]/route.ts
+++ app/api/proofs/[id]/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { s3Client } from '@/lib/awsClients';
-import { dummyData } from '@/scripts/dummyData.mjs';
 
 const prisma = new PrismaClient();
 
@@ -42,8 +41,6 @@ export async function DELETE(
       return NextResponse.json({ error: 'Proof not found' }, { status: 404 });
     }
 
-    // frontKey and backKey are the old fields that we're not using anymore
-    // dataImageS3Key is the new field that we're using
     const frontKey = proof.printFrontUrl?.split('/')?.pop();
     const backKey = proof.printBackUrl?.split('/')?.pop();
     const dataImageS3Key = proof.dataImageS3Key?.split('/')?.pop();
@@ -55,25 +52,17 @@ export async function DELETE(
       }));
     }
 
-    // This shouldn't hit anymore since we're not using the old fields
-    // Still preserving for now so the the old mock proofs stick around
     if (frontKey && backKey) {
-      // Don't delete the S3 objects if the URLs are from the dummy data
-      // Maybe this shouldn't be hardcoded like this, what's the worst that could happen?
-      if (dummyData.some(p => p.printFrontUrl === proof.printFrontUrl && p.printBackUrl === proof.printBackUrl)) {
-        console.log('Skipping deletion of dummy data');
-      } else {
-        await Promise.all([
-          s3Client.send(new DeleteObjectCommand({
-            Bucket: 'proofs-dashboard-dev',
-          Key: frontKey,
-        })),
+      await Promise.all([
         s3Client.send(new DeleteObjectCommand({
-            Bucket: 'proofs-dashboard-dev',
-            Key: backKey,
-          }))
-        ]);
-      }
+          Bucket: 'proofs-dashboard-dev',
+        Key: frontKey,
+      })),
+      s3Client.send(new DeleteObjectCommand({
+          Bucket: 'proofs-dashboard-dev',
+          Key: backKey,
+        }))
+      ]);
     }
 
     // Delete proof from database
